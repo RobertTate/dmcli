a3c4fa657e585cac81976edb72c9790f5a7290d2
RobertTate <robertjosephtate@gmail.com>
2022-01-21 20:50:22 -0600

    allow www when updating links

--- src/campaign_navigation/editContentItem.js
+++ src/campaign_navigation/editContentItem.js
@@ -105,7 +105,7 @@ async function editContentItem(datapath) {
             name: 'editedLink',
             message: `Enter new value for ${prettyStyle(linkToEdit)}:`,
             validate: (value) => {
-              const pass = value.match(/(http(s)?:\/\/.)?(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+              const pass = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
               if (pass) {
                 return true;
               }
